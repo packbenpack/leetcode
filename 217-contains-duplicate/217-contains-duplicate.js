5f4a6c02ee5f1d140c408179a61d04e8f90074b0
Ben Pack <benpack007@gmail.com>
2022-08-26 11:50:19 -0700

    Time: 137 ms (52.30%), Space: 50.8 MB (63.62%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,14 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let prev = {};
-    for (let i = 0; i < nums.length; i++) {
-        let currentNum = nums[i];
-        if (prev[currentNum] === undefined) {
-            prev[currentNum] = i
-        } else {
-            return true
-        }
-    }
-    return false
+    // let prev = {};
+    // for (let i = 0; i < nums.length; i++) {
+    //     let currentNum = nums[i];
+    //     if (prev[currentNum] === undefined) {
+    //         prev[currentNum] = i
+    //     } else {
+    //         return true
+    //     }
+    // }
+    // return false
+        return new Set(nums).size < nums.length;
+
 };
